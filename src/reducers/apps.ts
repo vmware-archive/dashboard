31c927ec7a433a07a4ae263101569725e959ebb6
Adnan Abdulhussein <adnan@bitnami.com>
2018-01-30 19:54:21 +0100

    add application list

---
+++ src/reducers/apps.ts
@@ -0,0 +1,23 @@
+import { getType } from "typesafe-actions";
+
+import actions from "../actions";
+import { AppsAction } from "../actions/apps";
+import { IAppState } from "../shared/types";
+
+const initialState: IAppState = {
+  isFetching: false,
+  items: [],
+};
+
+const appsReducer = (state: IAppState = initialState, action: AppsAction): IAppState => {
+  switch (action.type) {
+    case getType(actions.apps.requestApps):
+      return { ...state, isFetching: true };
+    case getType(actions.apps.receiveApps):
+      return { ...state, isFetching: false, items: action.apps };
+    default:
+  }
+  return state;
+};
+
+export default appsReducer;
