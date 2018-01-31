fd2143c655bc4b657c013f3ccb4e527549697bce
Sertaç Özercan <sozercan@gmail.com>
2018-01-31 11:15:19 -0800

    including icon

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -10,8 +10,8 @@ class Sidebar extends React.Component {
       <aside className="Sidebar bg-dark type-color-reverse-anchor-reset">
         <ul className="remove-style margin-reset padding-h-normal text-c">
           <li className="padding-v-normal">
-            <img src={placeholder} height="48" />
             <Link to="/">
+              <img src={placeholder} height="48" />
               <div className="type-small">Apps</div>
             </Link>
           </li>
