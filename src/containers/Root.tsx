9f157f5cf98153e6ffd3ea9277bd0fddd914cdcf
Adnan Abdulhussein <adnan@bitnami.com>
2018-02-08 10:15:03 +0100

    prettyfied release.d.ts

--- src/containers/Root.tsx
+++ src/containers/Root.tsx
@@ -25,7 +25,7 @@ class Root extends React.Component {
     [route: string]: React.ComponentType<RouteComponentProps<any>> | React.ComponentType<any>;
   } = {
     "/": AppList,
-    "/apps/:namespace/:releasename": AppView,
+    "/apps/:namespace/:releaseName": AppView,
     "/charts": ChartList,
     "/charts/:repo": ChartList,
     "/charts/:repo/:id": ChartView,
