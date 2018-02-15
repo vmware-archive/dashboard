b27137853f33df2334c7aadf3dac6351ed1419c7
Sertaç Özercan <sozercan@gmail.com>
2018-02-15 18:44:17 +0100

    checks for service catalog, service brokers and service instances

--- src/containers/InstanceListViewContainer.ts
+++ src/containers/InstanceListViewContainer.ts
@@ -3,6 +3,7 @@ import { Dispatch } from "redux";
 
 import actions from "../actions";
 import { InstanceListView } from "../components/InstanceListView";
+import { ServiceCatalog } from "../shared/ServiceCatalog";
 import { IStoreState } from "../shared/types";
 
 interface IRouteProps {
@@ -18,16 +19,25 @@ function mapStateToProps({ catalog }: IStoreState, { match: { params } }: IRoute
   const plans = catalog.plans;
   const classes = catalog.classes;
   const instances = catalog.instances;
+  const isInstalled = catalog.isInstalled;
   return {
     brokers,
     classes,
     instances,
+    isInstalled,
     plans,
   };
 }
 
 function mapDispatchToProps(dispatch: Dispatch<IStoreState>) {
   return {
+    checkCatalogInstalled: async () => {
+      const isInstalled = await ServiceCatalog.isCatalogInstalled();
+      isInstalled
+        ? dispatch(actions.catalog.installed())
+        : dispatch(actions.catalog.notInstalled());
+      return isInstalled;
+    },
     getCatalog: async () => {
       dispatch(actions.catalog.getCatalog());
     },
